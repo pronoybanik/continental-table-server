2cf312dcc6a166b9901a05012a42dad8264c4bd5
PRONOY <pronoybanik82@gmail.com>
2024-06-25 14:13:51 +0600

    add jwt token and verifecation

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require("cors")
 require("dotenv").config()
 const port = process.env.PORT || 5000;
-
+const jwt = require('jsonwebtoken');
 
 // middleware
 app.use(cors());
@@ -29,9 +29,92 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-        const menuCollection = client.db("continental-table-DB").collection("menu")
-        const reviewsCollection = client.db("continental-table-DB").collection("reviews")
-        const cartsCollection = client.db("continental-table-DB").collection("carts")
+        const menuCollection = client.db("continental-table-DB").collection("menu");
+        const usersCollection = client.db("continental-table-DB").collection("users");
+        const reviewsCollection = client.db("continental-table-DB").collection("reviews");
+        const cartsCollection = client.db("continental-table-DB").collection("carts");
+
+        // verify token  
+        const verifyToken = (req, res, next) => {
+
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: "unauthorized access" })
+            }
+            const token = req.headers.authorization.split(" ")[1]
+
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: "unauthorized access" })
+                }
+                req.decoded = decoded;
+                console.log("req.decoded", req.decoded);
+                next();
+            });
+        };
+
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            const isAdmin = user.role === "admin"
+            if (!isAdmin) {
+                res.status(403).send({ message: "forbidden access" })
+            }
+            next()
+        }
+
+
+
+
+        //jwt token 
+        app.post("/jwt", async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token });
+        });
+
+
+        // users api
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user)
+            res.send(result);
+        });
+        app.get("/users", verifyToken,verifyAdmin, async (req, res) => {
+            const result = await usersCollection.find().toArray()
+            res.send(result);
+        });
+        app.delete("/users/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await usersCollection.deleteOne(query);
+            res.send(result);
+        });
+        app.patch("/users/admin/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    role: "admin"
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc)
+            res.send(result)
+        });
+
+        app.get("/users/admin/:email", verifyToken, async (req, res) => {
+            const email = req.params.email
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: "Forbidden Access" })
+            }
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            let admin = false
+            if (user) {
+                admin = user?.role === "admin"
+            }
+            res.send({ admin })
+        })
 
         // Menu collection
         app.get("/menu", async (req, res) => {
@@ -51,7 +134,6 @@ async function run() {
             const result = await cartsCollection.insertOne(cartItem)
             res.send(result);
         });
-
         app.get("/carts", async (req, res) => {
             const email = req.query.email;
             const query = { email: email }
@@ -64,7 +146,6 @@ async function run() {
             const result = await cartsCollection.deleteOne(query);
             res.send(result);
         });
-
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
@@ -82,5 +163,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`continental server runing ${port}}`);
+    console.log(`continental server runIng ${port}}`);
 });
